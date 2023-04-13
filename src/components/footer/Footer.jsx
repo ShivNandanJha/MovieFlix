bae2576ea5b6f55915a99c014eeb4b7bdfe62ceb
Shiv Nandan Jha <neureto12@gmail.com>
2023-04-13 09:31:09 +0530

    links added to footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,60 @@
 import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaGithub, FaCodepen } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Movix is a movie searching app that lets users discover the latest
+          movies and TV shows with IMDb ratings. It also includes features to
+          watch trailers and check out the cast of films. With an intuitive
+          interface and advanced search options, Movix makes it easy for users
+          to find and enjoy their favorite movies and TV shows.
+          <hr />
+          <br />
+          <p> Made with love for all movie lovers ~ By Shivnandan Jha</p>
+          <p>Find me on</p>
+        </div>
+        <div className="socialIcons">
+          <a href="https://github.com/ShivNandanJha" target="_blank">
+            {" "}
+            <span className="icon">
+              <FaGithub />
+            </span>
+          </a>
+          <a href="https://codepen.io/Shivi-Code"  target="_blank" >
+            <span className="icon">
+              <FaCodepen />
+            </span>
+          </a>
+          <a href="https://www.linkedin.com/in/shiv-nandan-jha-4179a4251/" target="_blank">
+            <span className="icon">
+              <FaLinkedin />
+            </span>{" "}
+          </a>
+
+          <a href="https://www.instagram.com/probably_shiv/" target="_blank">
+            {" "}
+            <span className="icon">
+              <FaInstagram />
+            </span>
+          </a>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
